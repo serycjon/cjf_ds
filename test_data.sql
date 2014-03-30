a5d2744d1eeb230fa3bead8de766f2ad4004c0ab
serycjon <jonas.serych@gmail.com>
2014-03-30 21:09:00 +0200

    testing data initial commit

---
+++ test_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO staje(jmeno, mesto) VALUES ('Dukla', 'Pardubice');
+INSERT INTO staje(jmeno, mesto) VALUES ('Spolana', 'Neratovice');
+INSERT INTO staje(jmeno, mesto) VALUES ('KK Brno', 'Brno');
+
+INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Tomas', 'Vomacka', '1990-01-30');
+INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Karel', 'Zubrohlav', '2001-11-06');
+INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Rudolf', 'Nejedlý', '1930-04-11');
+INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Jenovéfa', 'Salámová', '1994-05-21');
+INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Jenovéfa', 'Salámová', '1965-02-12');
