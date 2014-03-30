8943a1521e5fc3e554bd75a8c6cadf0d66301132
serycjon <jonas.serych@gmail.com>
2014-03-30 23:00:12 +0200

    opravene defaultni hodnoty pro date a interval

--- cjf.sql
+++ cjf.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "zavod" (
 	"zavod_id" int NOT NULL UNIQUE DEFAULT nextval('seq_zavod'),
 	"nazev" varchar(50) NOT NULL,
-	"datum" date NOT NULL,
+	"datum" date NOT NULL default CURRENT_DATE,
 	"misto_konani" varchar(50) NOT NULL,
 	PRIMARY KEY("zavod_id")
 );
@@ -11,8 +11,8 @@ CREATE SEQUENCE seq_kategorie;
 CREATE TABLE "kategorie" (
 	"kategorie_id" int NOT NULL UNIQUE DEFAULT nextval('seq_kategorie'),
 	"nazev" varchar(20) NOT NULL,
-	"pocet_koni" int4 DEFAULT 1,
-	"pocet_prisedicich" int4 DEFAULT 1,
+	"pocet_koni" int4 DEFAULT 1 CHECK (pocet_koni > 0),
+	"pocet_prisedicich" int4 DEFAULT 1 CHECK (pocet_prisedicich >= 0),
 	PRIMARY KEY("kategorie_id")
 );
 
@@ -21,11 +21,12 @@ CREATE SEQUENCE seq_startovni_cislo;
 CREATE TABLE "tymy" (
 	"tym_id" int NOT NULL UNIQUE DEFAULT nextval('seq_tymy'),
 	"nazev" varchar(50) NOT NULL,
-	"startovni_cislo" int NOT NULL UNIQUE DEFAULT nextval('seq_startovni_cislo'),
-	"cas_prvniho_kola" interval(0),
-	"cas_druheho_kola" interval(0),
-	"penalizace_prvni_kolo" int4 NOT NULL DEFAULT 0,
-	"penalizace_druhe_kolo" int4 NOT NULL DEFAULT 0,
+	"startovni_cislo" int NOT NULL UNIQUE DEFAULT nextval('seq_startovni_cislo'), -- chceme udelat jakoby novou sekvenci pro kazdy zavod
+										      -- jeste jsme nevymysleli, jak na to
+	"cas_prvniho_kola" interval(0) NOT NULL DEFAULT '0',
+	"cas_druheho_kola" interval(0) NOT NULL DEFAULT '0',
+	"penalizace_prvni_kolo" int4 NOT NULL DEFAULT 0 CHECK (penalizace_prvni_kolo >= 0),
+	"penalizace_druhe_kolo" int4 NOT NULL DEFAULT 0 CHECK (penalizace_druhe_kolo >= 0),
 	"dojel" bool NOT NULL DEFAULT False,
 	"zavod_id" int4 NOT NULL,
 	"kategorie_id" int4 NOT NULL,
@@ -90,8 +91,8 @@ CREATE TABLE "tymy_has_kone" (
 CREATE TABLE "osoby_has_staje" (
 	"osoby_osoba_id" int4 NOT NULL,
 	"staje_staj_id" int4 NOT NULL,
-	"platne_od" date NOT NULL,
-	"platne_do" date NOT NULL,
+	"platne_od" date NOT NULL, -- zacatek tohoto roku, zatim jsme neprisli na to, jak ho tam dat defaultne
+	"platne_do" date NOT NULL, -- konec tohoto roku, zatim jsme neprisli na to, jak ho tam dat defaultne
 	PRIMARY KEY("osoby_osoba_id","staje_staj_id","platne_od","platne_do"),
 	FOREIGN KEY ("staje_staj_id") REFERENCES "staje"("staj_id"),
 	FOREIGN KEY ("osoby_osoba_id") REFERENCES "osoby"("osoba_id")
