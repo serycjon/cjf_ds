d09b09275690db06a05fcc6d1febf1f62da3b733
serycjon <jonas.serych@gmail.com>
2014-03-30 21:44:06 +0200

    kone, zavody, zavod_has_kategorie

--- test_data.sql
+++ test_data.sql
@@ -7,3 +7,23 @@ INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Karel', 'Zubrohlav',
 INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Rudolf', 'Nejedlý', '1930-04-11');
 INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Jenovéfa', 'Salámová', '1994-05-21');
 INSERT INTO osoby(jmeno, prijmeni, datum_narozeni) VALUES ('Jenovéfa', 'Salámová', '1965-02-12');
+
+INSERT INTO kategorie(nazev, pocet_koni, pocet_prisedicich) VALUES ('jednospřeží', 1, 1);
+INSERT INTO kategorie(nazev, pocet_koni, pocet_prisedicich) VALUES ('dvojspřeží', 2, 1);
+INSERT INTO kategorie(nazev, pocet_koni, pocet_prisedicich) VALUES ('čtyřspřeží', 4, 2);
+INSERT INTO kategorie(nazev, pocet_koni, pocet_prisedicich) VALUES ('dvanáctispřeží', 12, 6);
+
+INSERT INTO zavod(nazev, datum, misto_konani) VALUES ('První jarní vyjížďka', '2014-01-01', 'pole');
+INSERT INTO zavod(nazev, datum, misto_konani) VALUES ('Druhá jarní vyjížďka', '2014-02-01', 'louka');
+INSERT INTO zavod(nazev, datum, misto_konani) VALUES ('Třetí jarní vyjížďka', '2014-03-01', 'bazén');
+
+INSERT INTO zavod_has_kategorie(zavod_zavod_id, kategorie_kategorie_id)
+	SELECT zavod_id, kategorie_id FROM zavod, kategorie WHERE misto_konani = 'pole' AND kategorie.nazev = 'dvojspřeží' LIMIT 1;
+
+INSERT INTO zavod_has_kategorie(zavod_zavod_id, kategorie_kategorie_id)
+	SELECT zavod_id, kategorie_id FROM zavod, kategorie WHERE misto_konani = 'pole' AND kategorie.nazev = 'jednospřeží' LIMIT 1;
+
+INSERT INTO kone(jmeno, plemeno, majitel) VALUES ('Betty', 'pravá', 'Mr. Nobody');
+INSERT INTO kone(jmeno, plemeno, majitel) VALUES ('Fantom', 'Brumby', 'Mr. Everybody');
+INSERT INTO kone(jmeno, plemeno, majitel) VALUES ('Gaston', 'Gidran', 'Mr. Body');
+INSERT INTO kone(jmeno, plemeno, majitel) VALUES ('Hannibal', 'Hucul', 'Mr. Anybody');
