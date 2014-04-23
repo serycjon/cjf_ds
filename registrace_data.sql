b6d92d34c45d2006cfcc7cae058e0f89bf001e34
serycjon <jonas.serych@gmail.com>
2014-04-23 14:03:41 +0200

    added team BetaSamci, hromadnej insert

--- registrace_data.sql
+++ registrace_data.sql
@@ -3,28 +3,41 @@ INSERT INTO tymy (nazev, zavod_id, kategorie_id)
 	WHERE zavod.misto_konani = 'pole' AND kategorie.nazev = 'jednospřeží';
 
 INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
-	SELECT tym_id, osoba_id, true FROM tymy, osoby
-	WHERE tymy.nazev = 'AlfaSamci' AND osoby.jmeno = 'Karel' LIMIT 1;
-
-INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
-	SELECT tym_id, osoba_id, false FROM tymy, osoby
-	WHERE tymy.nazev = 'AlfaSamci' AND osoby.jmeno = 'Tomas' LIMIT 1;
+	SELECT * FROM (SELECT tym_id, osoba_id, true FROM tymy, osoby
+	WHERE tymy.nazev = 'AlfaSamci' AND osoby.jmeno = 'Karel' LIMIT 1) as jezdec
+	UNION
+	SELECT * FROM (SELECT tym_id, osoba_id, false FROM tymy, osoby
+	WHERE tymy.nazev = 'AlfaSamci' AND osoby.jmeno = 'Tomas' LIMIT 1) as prisedici1
+	UNION
+	SELECT * FROM (SELECT tym_id, osoba_id, true FROM tymy, osoby
+	WHERE tymy.nazev = 'AlfaSamci' AND osoby.jmeno = 'Rudolf' LIMIT 1) as jezdec2
+	UNION
+	SELECT * FROM (SELECT tym_id, osoba_id, false FROM tymy, osoby
+	WHERE tymy.nazev = 'AlfaSamci' AND osoby.datum_narozeni = '1994-05-21' LIMIT 1) as prisedici2;
 
 INSERT INTO tymy_has_kone (tymy_tym_id, kone_kun_id)
-	SELECT tym_id, kun_id FROM tymy, kone
-	WHERE tymy.nazev = 'AlfaSamci' AND kone.jmeno = 'Betty' LIMIT 1;
+	SELECT * FROM (SELECT tym_id, kun_id FROM tymy, kone
+	WHERE tymy.nazev = 'AlfaSamci' AND kone.jmeno = 'Betty' LIMIT 1) as kun1
+	UNION
+	SELECT * FROM (SELECT tym_id, kun_id FROM tymy, kone
+	WHERE tymy.nazev = 'AlfaSamci' AND kone.jmeno = 'Fantom' LIMIT 1) as kun2;
 
--- druhej jezdec - melo by to rvat!
+-------------------------------------------------------------------------
+
+INSERT INTO tymy (nazev, zavod_id, kategorie_id)
+	SELECT 'BetaSamci', zavod_id, kategorie_id FROM zavod, kategorie
+	WHERE zavod.misto_konani = 'pole' AND kategorie.nazev = 'dvojspřeží';
+
 INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
-	SELECT tym_id, osoba_id, true FROM tymy, osoby
-	WHERE tymy.nazev = 'AlfaSamci' AND osoby.jmeno = 'Rudolf' LIMIT 1;
+	SELECT * FROM (SELECT tym_id, osoba_id, true FROM tymy, osoby
+	WHERE tymy.nazev = 'BetaSamci' AND osoby.jmeno = 'Lada' LIMIT 1) as jezdec
+	UNION 
+	SELECT * FROM (SELECT tym_id, osoba_id, false FROM tymy, osoby
+	WHERE tymy.nazev = 'BetaSamci' AND osoby.jmeno = 'Jan' LIMIT 1) as prisedici;
 
--- moc prisedicich - melo by rvat!
-INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
-	SELECT tym_id, osoba_id, false FROM tymy, osoby
-	WHERE tymy.nazev = 'AlfaSamci' AND osoby.datum_narozeni = '1994-05-21' LIMIT 1;
-
--- moc koni - melo by rvat
 INSERT INTO tymy_has_kone (tymy_tym_id, kone_kun_id)
-	SELECT tym_id, kun_id FROM tymy, kone
-	WHERE tymy.nazev = 'AlfaSamci' AND kone.jmeno = 'Fantom' LIMIT 1;
+	SELECT * FROM (SELECT tym_id, kun_id FROM tymy, kone
+	WHERE tymy.nazev = 'BetaSamci' AND kone.jmeno = 'Stargate' LIMIT 1) as kun1
+	UNION
+	SELECT * FROM (SELECT tym_id, kun_id FROM tymy, kone
+	WHERE tymy.nazev = 'BetaSamci' AND kone.jmeno = 'Apollo' LIMIT 1) as kun2;
