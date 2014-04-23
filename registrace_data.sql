c5332c326a9f5110022f8fd54ca980ef0083d1f5
ondralad <ondralad@fel.cvut.cz>
2014-04-23 14:05:35 +0200

    Novy tym

--- registrace_data.sql
+++ registrace_data.sql
@@ -28,3 +28,40 @@ INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
 INSERT INTO tymy_has_kone (tymy_tym_id, kone_kun_id)
 	SELECT tym_id, kun_id FROM tymy, kone
 	WHERE tymy.nazev = 'AlfaSamci' AND kone.jmeno = 'Fantom' LIMIT 1;
+	
+	--moje
+	INSERT INTO tymy (nazev, zavod_id, kategorie_id)
+	SELECT 'Borci', zavod_id, kategorie_id FROM zavod, kategorie
+	WHERE zavod.misto_konani = 'Pod_susarnou' AND kategorie.nazev = 'jednospřeží';
+
+INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
+	SELECT tym_id, osoba_id, true FROM tymy, osoby
+	WHERE tymy.nazev = 'Borci' AND osoby.jmeno = 'Daniel' LIMIT 1;
+
+INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
+	SELECT tym_id, osoba_id, false FROM tymy, osoby
+	WHERE tymy.nazev = 'Borci' AND osoby.jmeno = 'Rudolf' LIMIT 1;
+
+INSERT INTO tymy_has_kone (tymy_tym_id, kone_kun_id)
+	SELECT tym_id, kun_id FROM tymy, kone
+	WHERE tymy.nazev = 'Borci' AND kone.jmeno = 'Fantom' LIMIT 1;
+	
+	--
+	INSERT INTO tymy (nazev, zavod_id, kategorie_id)
+	SELECT 'DrsnoJezdci', zavod_id, kategorie_id FROM zavod, kategorie
+	WHERE zavod.misto_konani = 'Pod_susarnou' AND kategorie.nazev = 'jednospřeží';
+
+INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
+	SELECT tym_id, osoba_id, true FROM tymy, osoby
+	WHERE tymy.nazev = 'DrsnoJezdci' AND osoby.jmeno = 'Jenovéfa' LIMIT 1;
+
+INSERT INTO tymy_has_osoby (tymy_tym_id, osoby_osoba_id, je_jezdec)
+	SELECT tym_id, osoba_id, false FROM tymy, osoby
+	WHERE tymy.nazev = 'DrsnoJezdci' AND osoby.jmeno = 'Michal' LIMIT 1;
+
+INSERT INTO tymy_has_kone (tymy_tym_id, kone_kun_id)
+	SELECT tym_id, kun_id FROM tymy, kone
+	WHERE tymy.nazev = 'DrsnoJezdci' AND kone.jmeno = 'Gaston' LIMIT 1;
+	
+	
+	
